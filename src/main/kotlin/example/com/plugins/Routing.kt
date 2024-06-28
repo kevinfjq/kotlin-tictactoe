473ffba498806207089b562c0eac274702312355
kevinfjq <kevifjq@gmail.com>
2024-06-27 21:24:50 -0300

    feat: add game route

--- src/main/kotlin/example/com/plugins/Routing.kt
+++ src/main/kotlin/example/com/plugins/Routing.kt
@@ -1,10 +1,12 @@
 package example.com.plugins
 
+import example.com.models.TicTacToeGame
+import example.com.socket
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
-fun Application.configureRouting() {
+fun Application.configureRouting(game: TicTacToeGame) {
     routing {
-
+        socket(game)
     }
 }
